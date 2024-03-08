294c2b3594212b93da43f1ac58b772e8a0cce648
Kerry Back <kerryback@gmail.com>
2024-03-07 19:52:45 -0600

    'message'

--- deck3.py
+++ deck3.py
@@ -15,6 +15,7 @@ files = [
 ]
 for file in files:
     os.system(f'decktape automatic docs\{file}.html docs\pdfs\{file}.pdf')
+
 os.system("git add .")
 os.system("git commit -m 'message'")
 os.system("git push origin main")
